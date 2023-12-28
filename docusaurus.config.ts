9c7896031015045d0a720186b48d23b84c15342e
dineug <dineug2@gmail.com>
2023-12-28 12:05:47 +0900

    fix: editUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -37,8 +37,7 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/dineug/erd-editor/tree/main/packages/erd-editor-docs/',
+          editUrl: 'https://github.com/dineug/erd-editor-docs/blob/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
