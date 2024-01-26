bdc6abf24864ef23285c6d2220fd8bc76abf001a
dineug <dineug2@gmail.com>
2024-01-26 14:39:04 +0900

    fix: algolia

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -111,7 +111,7 @@ const config: Config = {
     },
     algolia: {
       appId: 'OCAVZ00HZS',
-      apiKey: '574dd362836e83f52fb6011a7b7361aa',
+      apiKey: 'ff86c19945a7e7e36b22b12080a20490',
       indexName: 'erd-editor',
     },
   } satisfies Preset.ThemeConfig,
